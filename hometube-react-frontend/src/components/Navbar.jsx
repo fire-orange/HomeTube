0bf3632f4474a5c34457e675c8fac8aefa7d89a9
Orane Pereira <orange.milton@icloud.com>
2022-03-15 15:18:05 +0100

    implement Navbar

---
+++ hometube-react-frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="p-4 bg-zinc-800">
+      <h1 className=" text-white font-bold text-2xl">HomeTube</h1>
+    </div>
+  );
+};
+
+export default Navbar;
