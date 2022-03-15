079abc47eb62e4e4c48d1cd018465c0f19f7a281
Orane Pereira <orange.milton@icloud.com>
2022-03-15 15:12:18 +0100

    update breakpoints

--- hometube-react-frontend/tailwind.config.js
+++ hometube-react-frontend/tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '576px',
+
+      'md': '768px',
+
+      'lg': '1280px',
+
+      'xl': '1536px',
+
+      '2xl': '2160px',
+    }
   },
   plugins: [],
 }
