042a4679169a6a90075f69db8497606690de1830
Orane Pereira <orange.milton@icloud.com>
2022-03-17 14:23:41 +0100

    add search bar

--- hometube-react-frontend/src/components/Navbar.jsx
+++ hometube-react-frontend/src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Textfield from "./Textfield";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <div className="p-4 bg-zinc-800">
-      <h1 className=" text-white font-bold text-2xl">HomeTube</h1>
+    <div className="flex p-4 bg-zinc-800">
+      <h1 className=" text-white font-bold text-2xl grow">HomeTube</h1>
+      <div className="flex grow">
+        <Textfield
+          placeHolder="Search"
+          className=" text-white bg-black w-full rounded-r-none "
+        />
+        <Button className="rounded-l-none">Search</Button>
+      </div>
+      <div className="flex grow justify-end">
+        <div className="bg-orange-500 rounded-full w-10 h-10"></div>
+      </div>
     </div>
   );
 };
