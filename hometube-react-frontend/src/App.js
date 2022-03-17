8cd4f57527db39cfdbbe040d68c7b13e006cde1f
Orane Pereira <orange.milton@icloud.com>
2022-03-17 14:22:44 +0100

    implement react router; create home and login route

--- hometube-react-frontend/src/App.js
+++ hometube-react-frontend/src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <LoginPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<HomePage />} />
+          <Route path="login" element={<LoginPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
