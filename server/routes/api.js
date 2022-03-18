cef74627ce1d356a04fbe0549b54feeb0d92f0d7
Orane Pereira <orange.milton@icloud.com>
2022-03-18 18:29:48 +0100

    cleanup

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,21 +1,21 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
-router.get('/', function (req, res) {
-    res.send("API");
-})
+router.get("/", function (req, res) {
+  res.send("API");
+});
 
-router.get('/video', function (req, res) {
-    //get all videos detail
-})
+router.get("/video", function (req, res) {
+  //get all videos detail
+});
 
-router.get('/video/:videoId', function (req, res) {
-    //get video from req.params.videoId
-    res.send(req.params.videoId);
-})
+router.get("/video/:videoId", function (req, res) {
+  //get video from req.params.videoId
+  res.send(req.params.videoId);
+});
 
-router.get('/video/watch/:videoId', function (req, res) {
-    res.send("watch " + req.params.videoId);
-})
+router.get("/video/watch/:videoId", function (req, res) {
+  res.send("watch " + req.params.videoId);
+});
 
 module.exports = router;
